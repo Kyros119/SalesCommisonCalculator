0c41f59305d5794c2aa167850bed28a095574fde
Kyros119 <91588999+Kyros119@users.noreply.github.com>
2021-10-26 17:21:14 -0600

    Add project files.

---
+++ SalesCommisonCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesCommisonCalculator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
